73ecdd29fbd552aa5bbaa3edadf0220129078feb
Mels Vagharshyan <95281321+melsvagharshyan@users.noreply.github.com>
2022-10-09 12:28:57 +0400

    commit

--- src/Components/Header/HeaderText/HeaderText.tsx
+++ src/Components/Header/HeaderText/HeaderText.tsx
@@ -7,7 +7,7 @@ const HeaderText: React.FC = () => {
     return (
         <div className={styles.HeaderText}>
             <span className={styles.sloganText}>Development Is Great</span>
-            <h2>VAGHARSHYAN'S BLOG</h2>
+            <h2>VAGHARSHYAN'S BLOG1</h2>
             <span className={styles.description}>
               Dynamic Author Blog Page
             </span>
