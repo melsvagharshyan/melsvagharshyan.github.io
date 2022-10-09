316bc9e922e31e8ea92b6886613420218117787a
Mels Vagharshyan <95281321+melsvagharshyan@users.noreply.github.com>
2022-10-09 19:26:44 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import Skills from "./Components/Skills/Skills";
 import Messenger from "./Components/Messenger/Messenger";
 import Footer from "./Components/Footer/Footer";
 import FreeHelp from "./Components/FreeHelp/FreeHelp";
+import {useAppDispatch, useAppSelector} from "./Redux/hooks";
+import {setOpen} from "./Redux/Reducers/MobileReducer";
 
 
 
 const App: React.FC = () => {
-
-
+const {open} = useAppSelector(state=> state.open);
+const dispatch = useAppDispatch();
 
     return (
-        <div className={styles.App} >
+        <div className={styles.App} onClick={()=> open === true && dispatch(setOpen(false))} >
             <Header/>
             <About/>
             <Skills/>
